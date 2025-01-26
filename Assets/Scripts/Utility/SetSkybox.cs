5432b462c4c0d32e746a8fe8df034872f6712933
JustLoren <loren.naegle@gmail.com>
2025-01-26 13:00:02 -0600

    Added cool skybox stuff

---
+++ Assets/Scripts/Utility/SetSkybox.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SetSkybox : MonoBehaviour
+{
+    public Material skyboxMaterial;
+    private void OnEnable()
+    {     
+        if (skyboxMaterial != null)
+        {
+            RenderSettings.skybox = skyboxMaterial;
+            DynamicGI.UpdateEnvironment();
+        }
+    }
+}
